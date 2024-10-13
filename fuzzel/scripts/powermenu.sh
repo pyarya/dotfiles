ed01f2633f44319702a3389ad74a13bd8768ba0d
pyarya <aryan.aryal86@gmail.com>
2024-10-13 01:09:08 -0600

    fuzzel configs and switch to waybar

---
+++ fuzzel/scripts/powermenu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SELECTION="$(printf " lock\n󰍃 logout\n󰤄 suspend\n reboot\n reboot[UEFI]\n󰤆 poweroff" | fuzzel --dmenu -l 6 -p "󰐦 " --no-exit-on-keyboard-focus-loss --config=/home/chopper/.config/fuzzel/scripts/keys.ini)"
+
+case $SELECTION in
+	*"lock")
+		~/.config/sway/brightness_lock.sh;;
+	*"suspend")
+		systemctl suspend;;
+	*"logout")
+		swaymsg exit;;
+	*"reboot")
+		systemctl reboot;;
+	*"reboot[uefi]")
+		systemctl reboot --firmware-setup;;
+	*"poweroff")
+		systemctl poweroff;;
+esac
