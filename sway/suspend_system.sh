a24f5226e5a6eb4270a9ab3ce49944517d5a63f3
pyarya <aryan.aryal86@gmail.com>
2024-10-17 00:36:16 -0600

    sway conf changes

--- sway/suspend_system.sh
+++ sway/suspend_system.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [[ "$(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}')" == open ]]
+if [[ "$(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}')" == closed ]]
 then
   sudo /usr/local/bin/send_sleep_email.sh
   systemctl suspend
