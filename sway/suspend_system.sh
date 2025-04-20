c6b2d5f8e5d7eb4ccc67894d6afd4b337ea4eec7
pyarya <aryan.aryal86@gmail.com>
2025-04-20 13:03:18 -0600

    remove sway leftovers

--- sway/suspend_system.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [[ "$(cat /proc/acpi/button/lid/LID0/state | awk '{print $2}')" == closed ]]
-then
-  sudo /usr/local/bin/send_sleep_email.sh
-  systemctl suspend
-fi
--- sway/toggle_fcitx.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CURRENT_ID="$(pgrep ^fcitx5$)"
-
-if [[ -n "$CURRENT_ID" ]]; then
-  kill "$CURRENT_ID"
-else
-  fcitx5 -d --replace
-fi
